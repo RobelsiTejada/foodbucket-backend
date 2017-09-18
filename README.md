9accb9c86ced3083f3646e027a5fd7d5a225e71f
maccode421 <maccavalli@yahoo.com>
2017-09-18 15:24:04 -0400

    Updated README

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 :Kyle Pearson :Rob Tejada :Macc Pholsina
 
 # Description of Application
-
-
+A bucket list that lets you keep track of the all the restaurants you want to go to in your city.
 
 # API routes
+Yelp! API Documentation. https://www.yelp.com/developers/documentation/v3
 
+Express API connects to Heroku back-end.
 
 
 # Links
-
 *deployed front-end app
 
-*back-end repo
+Heroku link:
+https://foodbucket.herokuapp.com/
--- scripts/restaurants/show.sh
+++ scripts/restaurants/show.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-curl \
-  -X GET \
-  -H "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
-  "https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=41.792875&longitude=-71.414163&radius=8047"
+API="https://api.yelp.com/v3"
+URL_PATH="/businesses/search"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request GET \
+  --header "Authorization: Bearer NHG0larijXXaQKiCWF1D7zz_vpxcnFDPMoIz-i1tRcxdx3Af18IVCYYLXfQGes0o_0R-2TiXKv3qHYs981_sNJiE3yjYZJAkDGEuqNKmzBpZuaxeaBnjF_PtQWC9WXYx" \
+  --header "Authorization: Token token=$TOKEN"
+  --data '{
+    "search": {
+      "latitude": "'"${LATITUDE}"'",
+      "longitude": "'"${LONGITUDE}"'"
+        }
+}'
 
 echo
