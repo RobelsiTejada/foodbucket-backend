270808204e37e871f71323af6a698e4eb5efe736
RobelsiTejada <robelsitejada@gmail.com>
2017-09-29 11:34:58 -0400

    testing json parse

---
+++ app/models/businesses.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const request = require('request-promise')
+
+const baseUrl = 'https://api.yelp.com/v3/'
+
+class Yelpv3 {
+  constructor (opts) {
+    this.appId = opts.app_id
+    this.appSecret = opts.app_secret
+    this.accessToken = ''
+  }
+
+  getAccessToken () {
+    if (this.accessToken) {
+      return Promise.resolve(this.accessToken)
+    } else {
+      return request({
+        method: 'POST',
+        uri: 'https://api.yelp.com/oauth2/token',
+        form: {
+          client_id: this.appId,
+          client_secret: this.appSecret,
+          grant_type: 'client_credentials'
+        }
+      }).then((response) => {
+        this.accessToken = JSON.parse(response).access_token
+        return this.accessToken
+      })
+    }
+  }
+
+  get (resource, params) {
+    params = (typeof params === 'undefined') ? {} : params
+
+    return this.getAccessToken().then((token) => {
+      return request({
+        uri: baseUrl + resource + (params),
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      }).then((response) => {
+        return response
+      }).catch((err) => {
+        // Handles expired access token
+        if (err.statusCode === 401) {
+          this.accessToken = null
+          return this.get(resource, params)
+        }
+        throw err
+      })
+    })
+  }
+
+  search (params) {
+    return this.get('businesses/search', params)
+  }
+
+  business (id) {
+    return this.get(`businesses/${id}`, undefined)
+  }
+}
+
+module.exports = Yelpv3
