2a756cc97bcfd1ba9cdf2a32bc718d0185ba4e74
RobelsiTejada <robelsitejada@gmail.com>
2017-10-01 11:42:03 -0400

    fixing list

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -3,19 +3,35 @@
 const controller = require('lib/wiring/controller')
 const models = require('app/models')
 const List = models.list
+
 const authenticate = require('./concerns/authenticate')
 const setUser = require('./concerns/set-current-user')
 const setModel = require('./concerns/set-mongoose-model')
 
+const index = (req, res, next) => {
+  List.find()
+    .then(list => res.json({
+      list: list.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user }))
+    }))
+    .catch(next)
+}
+
 const show = (req, res) => {
   res.json({
-    list: req.List.toJSON({ virtuals: true, user: req.user })
+    list: req.list.toJSON({ virtuals: true, user: req.user })
   })
 }
 
 const update = (req, res, next) => {
   delete req.body._owner  // disallow owner reassignment.
-  req.List.update(req.body.List)
+  req.list.update(req.body.list)
+    .then(() => res.sendStatus(204))
+    .catch(next)
+}
+
+const destroy = (req, res, next) => {
+  req.list.remove()
     .then(() => res.sendStatus(204))
     .catch(next)
 }
@@ -34,12 +50,14 @@ const create = (req, res, next) => {
 }
 
 module.exports = controller({
+  index,
   show,
   update,
+  destroy,
   create
 }, { before: [
-  { method: setUser, only: ['show'] },
-  { method: authenticate, except: ['show'] },
-  { method: setModel(List), only: ['show', 'update', 'create'] },
-  { method: setModel(List, { forUser: true }), only: ['show'] }
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(List), only: ['show'] },
+  { method: setModel(List, { forUser: true }), only: ['update', 'destroy'] }
 ] })
--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -3,31 +3,41 @@
 const controller = require('lib/wiring/controller')
 const models = require('app/models')
 const Restaurants = models.restaurants
+
 const authenticate = require('./concerns/authenticate')
 const setUser = require('./concerns/set-current-user')
 const setModel = require('./concerns/set-mongoose-model')
 
+const index = (req, res, next) => {
+  Restaurants.find()
+    .then(restaurants => res.json({
+      restaurants: restaurants.map((e) =>
+        e.toJSON({ virtuals: true, user: req.user }))
+    }))
+    .catch(next)
+}
+
 const show = (req, res) => {
   res.json({
-    restaurants: req.Restaurants.toJSON({ virtuals: true, user: req.user })
+    restaurants: req.restaurants.toJSON({ virtuals: true, user: req.user })
   })
 }
 
 const update = (req, res, next) => {
   delete req.body._owner  // disallow owner reassignment.
-  req.Restaurants.update(req.body.Restaurants)
+  req.restaurants.update(req.body.restaurants)
     .then(() => res.sendStatus(204))
     .catch(next)
 }
 
 const destroy = (req, res, next) => {
-  req.Restaurants.remove()
+  req.restaurants.remove()
     .then(() => res.sendStatus(204))
     .catch(next)
 }
 
 const create = (req, res, next) => {
-  const restaurants = Object.assign(req.body.Restaurants, {
+  const restaurants = Object.assign(req.body.restaurants, {
     _owner: req.user._id
   })
   Restaurants.create(restaurants)
@@ -40,13 +50,14 @@ const create = (req, res, next) => {
 }
 
 module.exports = controller({
+  index,
   show,
   update,
   destroy,
   create
 }, { before: [
-  { method: setUser, only: ['show'] },
-  { method: authenticate, except: ['show'] },
-  { method: setModel(Restaurants), only: ['show', 'update', 'destroy', 'create'] },
-  { method: setModel(Restaurants, { forUser: true }), only: ['show'] }
+  { method: setUser, only: ['index', 'show'] },
+  { method: authenticate, except: ['index', 'show'] },
+  { method: setModel(Restaurants), only: ['show'] },
+  { method: setModel(Restaurants, { forUser: true }), only: ['update', 'destroy'] }
 ] })
