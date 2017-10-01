58dbe16320ef87d55598d4fd868863c073978e9f
RobelsiTejada <robelsitejada@gmail.com>
2017-10-01 18:54:06 -0400

    fixing list

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -2,7 +2,7 @@
 
 const controller = require('lib/wiring/controller')
 const models = require('app/models')
-const Restaurants = models.restaurants
+const Restaurants = models.restaurant
 
 const authenticate = require('./concerns/authenticate')
 const setUser = require('./concerns/set-current-user')
