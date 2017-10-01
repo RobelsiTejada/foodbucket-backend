36281ac8d8e7fd41aa3210133aab4a62599e11ee
RobelsiTejada <robelsitejada@gmail.com>
2017-10-01 17:24:04 -0400

    fixing list

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -9,7 +9,7 @@ const setUser = require('./concerns/set-current-user')
 const setModel = require('./concerns/set-mongoose-model')
 
 const index = (req, res, next) => {
-  Restaurants.find()
+  Restaurants.find({})
     .then(restaurants => res.json({
       restaurant: restaurants.map((e) =>
         e.toJSON({ virtuals: true, user: req.user }))
